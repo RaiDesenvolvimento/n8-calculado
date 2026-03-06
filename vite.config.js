81a42e7a9af6949f39d0c9287b0a4523b881715f
RdfSistema <rdf.sistema@gmail.com>
2026-03-06 12:46:15 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -11,7 +10,8 @@ export default defineConfig({
     }),
   ],
   server: {
-    host: '0.0.0.0', 
-    port: 5173,     
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['calc.n8inc.com.br']
   },
 })
